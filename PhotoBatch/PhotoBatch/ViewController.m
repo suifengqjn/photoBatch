a71cfc24a25a6a1ebd2afc9b1dd8be0bddd22d10
qianjianeng <329426491@qq.com>
2017-11-29 16:00:20 +0800

    获取文件，文件夹路径

--- PhotoBatch/PhotoBatch/ViewController.m
+++ PhotoBatch/PhotoBatch/ViewController.m
@@ -16,6 +16,30 @@
     
 }
 
+- (IBAction)addFile:(NSButton *)sender {
+    
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    [openPanel setPrompt: @"打开"];
+    [openPanel setCanChooseDirectories:YES]; //设置允许打开文件夹
+    [openPanel setAllowsMultipleSelection:YES]; // 会否允许打开多个目录
+    [openPanel setCanChooseFiles:YES];  //设置允许打开文件
+    [openPanel setCanCreateDirectories:YES]; // 允许新建文件夹
+    [openPanel setCanDownloadUbiquitousContents:NO]; //是否处理还未下载成功的文档
+    [openPanel setCanResolveUbiquitousConflicts:NO]; //是否处理有冲突的文档
+    openPanel.allowedFileTypes = [NSArray arrayWithObjects: @"jpg", @"doc",@"txt",@"jpeg",@"png",@"tiff", nil]; //设置允许打开的文件类型
+    [openPanel beginSheetModalForWindow:[NSApplication sharedApplication].keyWindow completionHandler:^(NSModalResponse result) {
+        
+        NSArray *filePaths = [openPanel URLs];
+        NSLog(@"-----%@", filePaths);
+        
+    }];
+    
+    
+    
+    
+    
+    
+}
 
 
 
